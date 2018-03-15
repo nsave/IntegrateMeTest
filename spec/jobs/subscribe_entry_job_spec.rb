4cab2e7134412538b60af0f809a8ec26efbd922f
nsave <nick.saveljev@gmail.com>
2018-03-15 18:36:49 +0300

    Update job specs

--- spec/jobs/subscribe_entry_job_spec.rb
+++ spec/jobs/subscribe_entry_job_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SubscribeEntryJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:entry) { double(:entry) }
+
+  describe '#perform' do
+    it 'delegates to EntrySubscriptionService' do
+      ActiveJob::Base.queue_adapter = :test
+
+      service = double(:service)
+      expect(Services::SubscribeEntryService).to receive(:new).with(entry).and_return(service)
+      expect(service).to receive(:run)
+
+      described_class.perform_now(entry)
+    end
+  end
 end
